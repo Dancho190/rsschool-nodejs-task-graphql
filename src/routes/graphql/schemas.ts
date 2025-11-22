b989c9d7bd4865d05e2f3d28cd3840ff157f0c87
Dancho190 <daniallebrasko@gmail.com>
2025-11-22 12:09:28 +0600

    feat:initial commit,first changes.

--- src/routes/graphql/schemas.ts
+++ src/routes/graphql/schemas.ts
@@ -1,4 +1,124 @@
 import { Type } from '@fastify/type-provider-typebox';
+import {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLList,
+  GraphQLNonNull,
+  GraphQLInputObjectType,
+  GraphQLFloat,
+  GraphQLBoolean,
+  GraphQLInt,
+} from 'graphql';
+import { UUIDType } from './types/uuid.js';
+
+export const UserType: GraphQLObjectType = new GraphQLObjectType({
+  name: 'User',
+  fields: () => ({
+    id: { type: new GraphQLNonNull(UUIDType) },
+    name: { type: new GraphQLNonNull(GraphQLString) },
+    balance: { type: new GraphQLNonNull(GraphQLFloat) },
+    profile: { type: ProfileType },
+    posts: { type: new GraphQLList(PostType) },
+    userSubscribedTo: { type: new GraphQLList(SubscriberType) },
+    subscribedToUser: { type: new GraphQLList(SubscriberType) },
+  }),
+});
+
+export const ProfileType: GraphQLObjectType = new GraphQLObjectType({
+  name: 'Profile',
+  fields: () => ({
+    id: { type: new GraphQLNonNull(UUIDType) },
+    isMale: { type: new GraphQLNonNull(GraphQLBoolean) },
+    yearOfBirth: { type: new GraphQLNonNull(GraphQLInt) },
+    userId: { type: new GraphQLNonNull(UUIDType) },
+    memberTypeId: { type: new GraphQLNonNull(UUIDType) },
+    user: { type: UserType },
+    memberType: { type: MemberTypeType },
+  }),
+});
+
+export const MemberTypeType: GraphQLObjectType = new GraphQLObjectType({
+  name: 'MemberType',
+  fields: () => ({
+    id: { type: new GraphQLNonNull(UUIDType) },
+    discount: { type: new GraphQLNonNull(GraphQLFloat) },
+    postsLimitPerMonth: { type: new GraphQLNonNull(GraphQLInt) },
+    profiles: { type: new GraphQLList(ProfileType) },
+  }),
+});
+
+export const PostType: GraphQLObjectType = new GraphQLObjectType({
+  name: 'Post',
+  fields: () => ({
+    id: { type: new GraphQLNonNull(UUIDType) },
+    title: { type: new GraphQLNonNull(GraphQLString) },
+    content: { type: new GraphQLNonNull(GraphQLString) },
+    authorId: { type: new GraphQLNonNull(UUIDType) },
+    author: { type: UserType },
+  }),
+});
+
+export const SubscriberType: GraphQLObjectType = new GraphQLObjectType({
+  name: 'SubscriberType',
+  fields: () => ({
+    subscriberId: { type: new GraphQLNonNull(UUIDType) },
+    authorId: { type: new GraphQLNonNull(UUIDType) },
+    subscriber: { type: UserType },
+    author: { type: UserType },
+  }),
+});
+
+export const CreateUserInputType = new GraphQLInputObjectType({
+  name: 'CreateUserInput',
+  fields: {
+    name: { type: new GraphQLNonNull(GraphQLString) },
+    balance: { type: new GraphQLNonNull(GraphQLFloat) },
+  },
+});
+
+export const CreateProfileInputType = new GraphQLInputObjectType({
+  name: 'CreateProfileInput',
+  fields: {
+    isMale: { type: new GraphQLNonNull(GraphQLBoolean) },
+    yearOfBirth: { type: new GraphQLNonNull(GraphQLInt) },
+    userId: { type: new GraphQLNonNull(UUIDType) },
+    memberTypeId: { type: new GraphQLNonNull(UUIDType) },
+  },
+});
+
+export const CreatePostInputType = new GraphQLInputObjectType({
+  name: 'CreatePostInput',
+  fields: {
+    title: { type: new GraphQLNonNull(GraphQLString) },
+    content: { type: new GraphQLNonNull(GraphQLString) },
+    authorId: { type: new GraphQLNonNull(UUIDType) },
+  },
+});
+
+export const ChangeUserInputType = new GraphQLInputObjectType({
+  name: 'ChangeUserInput',
+  fields: {
+    name: { type: GraphQLString },
+    balance: { type: GraphQLFloat },
+  },
+});
+
+export const ChangeProfileInputType = new GraphQLInputObjectType({
+  name: 'ChangeProfileInput',
+  fields: {
+    isMale: { type: GraphQLBoolean },
+    yearOfBirth: { type: GraphQLInt },
+    memberTypeId: { type: UUIDType },
+  },
+});
+
+export const ChangePostInputType = new GraphQLInputObjectType({
+  name: 'ChangePostInput',
+  fields: {
+    title: { type: GraphQLString },
+    content: { type: GraphQLString },
+  },
+});
 
 export const gqlResponseSchema = Type.Partial(
   Type.Object({
@@ -12,6 +132,7 @@ export const createGqlResponseSchema = {
     {
       query: Type.String(),
       variables: Type.Optional(Type.Record(Type.String(), Type.Any())),
+      operationName: Type.Optional(Type.String()),
     },
     {
       additionalProperties: false,
